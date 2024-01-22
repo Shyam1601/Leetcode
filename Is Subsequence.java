45599c6e84fd3a7c04f832f4990d576b45b2a189
Shyam1601 <132558139+Shyam1601@users.noreply.github.com>
2024-01-22 23:45:11 +0530

    Create Is Subsequence.java

---
+++ Is Subsequence.java
@@ -0,0 +1,40 @@
+/*392. Is Subsequence
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.*/
+
+//Code:-
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length()) return false;
+
+        int i=0,j=0;
+        while(j<t.length() && i<s.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+}
